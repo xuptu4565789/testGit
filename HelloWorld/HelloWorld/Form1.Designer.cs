5c0f626940af2b45484a4859866c4d8b8167d33a
REX.CY.LIN <REX.CY.LIN@deltaww.com>
2022-09-06 11:47:14 +0800

    add as below: 1. add a label on the form

--- HelloWorld/HelloWorld/Form1.Designer.cs
+++ HelloWorld/HelloWorld/Form1.Designer.cs
@@ -28,13 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lb_HI = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lb_HI
+            // 
+            this.lb_HI.AutoSize = true;
+            this.lb_HI.Location = new System.Drawing.Point(195, 86);
+            this.lb_HI.Name = "lb_HI";
+            this.lb_HI.Size = new System.Drawing.Size(86, 18);
+            this.lb_HI.TabIndex = 0;
+            this.lb_HI.Text = "hello world";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lb_HI);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lb_HI;
     }
 }
 
