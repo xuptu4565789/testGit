108b0d846d3c4784b9d8908278dad92791569194
REX.CY.LIN <REX.CY.LIN@deltaww.com>
2022-09-06 13:51:16 +0800

    add as below: create one label

--- HelloWorld/HelloWorld/Form1.Designer.cs
+++ HelloWorld/HelloWorld/Form1.Designer.cs
@@ -29,22 +29,33 @@
         private void InitializeComponent()
         {
             this.lb_HI = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lb_HI
             // 
             this.lb_HI.AutoSize = true;
-            this.lb_HI.Location = new System.Drawing.Point(195, 86);
+            this.lb_HI.Location = new System.Drawing.Point(182, 68);
             this.lb_HI.Name = "lb_HI";
             this.lb_HI.Size = new System.Drawing.Size(86, 18);
             this.lb_HI.TabIndex = 0;
             this.lb_HI.Text = "hello world";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(171, 261);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "hello world";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.lb_HI);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +67,7 @@
         #endregion
 
         private System.Windows.Forms.Label lb_HI;
+        private System.Windows.Forms.Label label1;
     }
 }
 
